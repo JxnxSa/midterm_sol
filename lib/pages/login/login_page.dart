8a059dacd42edab22062b0311a0a8d99363a25fa
Sarawalee Tangjittham <tangjittham_s@silpakorn.edu>
2022-09-18 14:59:24 +0700

    update4

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:midterm_sol/pages/main/main_page.dart';
+import 'package:midterm_sol/utils/helper.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -10,7 +12,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   var _input = '';
-  Widget _buildIndicator(bool on){
+  //var _message = '';
+  Widget _buildIndicator(bool on) {
     return Padding(
       padding: const EdgeInsets.all(4.0),
       child: Container(
@@ -28,14 +31,36 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  void _handleClickButton(int num){
+  void _handleClickButton(int num) {
+    const password = '123456';
     setState(() {
-      _input = _input + num.toString();
+      if (num == -1) {
+        _input = _input.substring(0, _input.length - 1);
+      } else {
+        _input = _input + num.toString();
+      }
     });
+    if (_input.length == 6) {
+      //ตรวจสอบรหัสผ่านว่าถูกต้องหรือไม่
+      if (_input == password) {
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => const MainPage()
+        )
+        );
+        // setState(() {
+        //   _message = 'ยินดีต้อนรับสู่ Mobile banking';
+        // });
+      } else {
+        showMyDialog(context, 'Sorry', 'Incorrect PIN ,Please try again');
+        // setState(() {
+        //   _message = 'รหัสผ่านไม่ถูกต้อง กรุณากรอกใหม่';
+        // });
+      }
+    }
   }
 
   // การสร้างฟังก์ชันในรูปแบบ function declaration
-  void func1(){
+  void func1() {
     var myList = [1, 2, 3, 'Hello'];
   }
 
@@ -44,13 +69,12 @@ class _LoginPageState extends State<LoginPage> {
     //
   };
 
-  void func3(){
+  void func3() {
     func1();
     func2();
     var f = func2;
     f();
   }
-
 
   Widget _buildNumberButton(int num) {
     return Padding(
@@ -61,7 +85,7 @@ class _LoginPageState extends State<LoginPage> {
         //     // _input = _input + num.toString();
         //   });
         // },
-        onTap: (){
+        onTap: () {
           _handleClickButton(num);
         },
         customBorder: CircleBorder(),
@@ -71,22 +95,27 @@ class _LoginPageState extends State<LoginPage> {
           alignment: Alignment.center,
           decoration: BoxDecoration(
             shape: BoxShape.circle,
-            border: Border.all(
-              color: Color(0xFF333333),
-              width: 1.0,
-            ),
+            border: num != -1
+                ? Border.all(
+                    color: Color(0xFF333333),
+                    width: 1.0,
+                  )
+                : null,
           ),
-          child: Text(
-            num.toString(),
-            style: GoogleFonts.firaCode(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+          child: num == -1
+              ? Icon(Icons.backspace_outlined)
+              : Text(
+                  num.toString(),
+                  style: GoogleFonts.firaCode(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
         ),
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     var numberRow1 = [
@@ -94,6 +123,17 @@ class _LoginPageState extends State<LoginPage> {
       _buildNumberButton(2),
     ];
     numberRow1.add(_buildNumberButton(3));
+
+    var myList1 = [1, 2, 3];
+    /*var myList2 = [];
+    for(var i = 0; i < myList1.length; i++){
+      myList2.add(myList1[i]);
+    }
+    myList2.add(4);
+    */
+
+    // Spread operator(...)
+    var myList2 = [0, ...myList1, 4, 5, 6];
 
     /*var myList = <Widget>[];
     for(var i = 4; i <= 6; i++){
@@ -107,13 +147,12 @@ class _LoginPageState extends State<LoginPage> {
     var status = false;
     var list = [
       0,
-      for(var i = 1; i <= 100; i++)
-          i,
-      101,102,103,
-      if(status) 104,
+      for (var i = 1; i <= 100; i++) i,
+      101,
+      102,
+      103,
+      if (status) 104,
     ];
-
-
 
     return Scaffold(
       body: Column(
@@ -152,27 +191,30 @@ class _LoginPageState extends State<LoginPage> {
                 // children: myList,
                 children: [
                   //collection for
-                  for(var i = 4; i <= 6; i++)
-                    _buildNumberButton(i)
+                  for (var i = 4; i <= 6; i++) _buildNumberButton(i)
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  _buildNumberButton(7),
-                  _buildNumberButton(8),
-                  _buildNumberButton(9),
+                  for (var i in [7, 8, 9]) _buildNumberButton(i)
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  SizedBox(
+                    width: 76.0,
+                    height: 76.0,
+                  ),
                   _buildNumberButton(0),
+                  _buildNumberButton(-1),
                 ],
               )
             ],
           ),
-          Text(_input),
+          // Text(_input),
+          // Text(_message),
         ],
       ),
     );
